440f5aed48b2d28e9999dcf964991ccb6f573080
nika_chorna <veronika.chorna2000@gmail.com>
2023-01-24 16:26:20 +0200

    add functions

--- script/main.js
+++ script/main.js
@@ -1,80 +1,20 @@
-// const button = document.querySelector("#submit-btn");
-// const removeBtn = document.querySelector("#remove");
 
-// const bgColors = ["red", "blue", "aqua", "green"];
-
-// // button.onclick = onBtnPress;
-
-// function onButtonClick(e) {
-//   setBtnStyles(e.target);
-// }
-
-// function setBtnStyles(el) {
-//   const randomColor = Math.floor(Math.random() * (bgColors.length + 1));
-
-//   el.style.backgroundColor = bgColors[randomColor];
-// }
-
-// button.addEventListener("click", onButtonClick);
-
-// removeBtn.addEventListener("click", () => {
-//   button.style.display = "none";
-//   button.removeEventListener("click", onButtonClick);
-// });
-
-// ----------------------------------------------------------------------------------------------------------------
-
-
-// const input = document.querySelector("input");
 // const btn = document.querySelector("button");
 
-// let inputValue = "";
 
-// input.addEventListener("input", function (e) {
-//   inputValue = e.target.value;
-// });
+// let isWButtonPressed = false;
 
-// btn.addEventListener("click", () => {
-//   checkIsInputValid(input, validateEmail(inputValue));
-// });
-
-// function validateEmail(email) {
-//   return String(email)
-//     .toLowerCase()
-//     .match(
-//       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-//     );
-// }
-
-// function checkIsInputValid(el, regExTest) {
-//   if (regExTest == null) {
-//     el.style.border = "2px solid red";
+// document.addEventListener("keydown", (e) => {
+//   if (isWButtonPressed) {
 //     return;
 //   }
 
-//   el.style.border = "2px solid gray";
-// }
+//   if (e.code === "KeyW") {
+//     isWButtonPressed = true;
+//   }
 
-
-// -----------------------------------------------------------------------------------------------------------------
-
-// const input = document.querySelector("input");
-// const btn = document.querySelector("button");
-
-// let inputValue = "";
-let isWButtonPressed = false;
-// ------------------------"keyup" 
-document.addEventListener("keydown", (e) => {
-  if (isWButtonPressed) {
-    return;
-  }
-
-  if (e.code === "KeyW") {
-    isWButtonPressed = true;
-  }
-
-  console.log("your pressed key: ", { e });
-});
+//   console.log("your pressed key: ", { e });
+// });
 
 // document.addEventListener("keyup", (e) => {
 //   if (e.code === "KeyW") {
@@ -83,29 +23,38 @@ document.addEventListener("keydown", (e) => {
 
 //   console.log("your pressed key: ", { e });
 // });
+const seasons = {
+  "rain": 'url("./images/rain_forest.jpg")',
+  "heavy-rain": 'url("./images/heavy_rain.jpg")',
+  "thunder": 'url("./images/thunder.jpg"',
+  "birds": 'url("./images/birds.png"',
+  "forest": 'url("./images/night_forest.jpg"',
+}
+
+function updatePage(valueId) {
+  let link = document.getElementById('container');
+  link.style.background = seasons[valueId];
+  link.style.backgroundSize = 'cover';
+  document.querySelectorAll('audio').forEach(element => {
+    element.pause();
+  });
+  document.getElementById('sound-' + valueId).play();
+}
 
 
-// -----------------------------------------------------------------------------------------------------------------
-// const input = document.querySelector("input");
-// const btn = document.querySelector("button");
 
-// let inputValue = "";
-// let isButtonPressed = false;
+document.addEventListener("keydown", (e) => {
+  if(e.code === "KeyA") {
+    updatePage('rain');
+  } else if(e.code === "KeyS"){
+    updatePage('heavy-rain');
+  } else if(e.code === "KeyD"){
+    updatePage('thunder');
+  } else if(e.code === "KeyF"){
+    updatePage('birds');
+  } else if(e.code === "KeyG"){
+    updatePage('forest');
+  } 
+});
 
-// btn.addEventListener("click", () => {
-//   isButtonPressed = true;
-//   btn.style.position = "absolute";
-// });
 
-// document.addEventListener("mousemove", function (event) {
-//   if (isButtonPressed) {
-//     mouseMoove(event.clientX, event.clientY);
-//   }
-// });
-
-// function mouseMoove(x, y) {
-//   btn.style.left = x + "px";
-//   btn.style.top = y + "px";
-// }
-
-// -----------------------------------------------------------------------------------------------------------------
